9b8403b8eef53a993dee3009a9d6951271095e78
User <user@MacBook-Pro-Stanislav.local>
2024-01-03 09:16:12 +0300

    Application for PHAT shop. Saint-Petersburg.

---
+++ src/main/java/ru/phatshop/PhatShopApplication.java
@@ -0,0 +1,13 @@
+package ru.phatshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhatShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhatShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/phatshop/PhatShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.phatshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhatShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
